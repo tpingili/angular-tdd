a05a3afcdf71a03aeeded85429d2ddf107928927
Tejaswini Pingili <ptejaswini.reddy@gmail.com>
2015-07-01 09:44:14 -0500

    added beforeEach function and introduced httpBackend

--- test/main.spec.js
+++ test/main.spec.js
@@ -4,15 +4,23 @@ var expect = chai.expect;
 describe('The Address Book App', function(){
   describe('The Contact Service', function(){
 
-    it('should have a property contacts, an array', function(){
+    beforeEach(function(){
       module('AddressBook');
       inject(function($injector){
         contactService = $injector.get('contactService');
+        $httpBackend = $injector.get('$httpBackend');
       });
-
-      expect(contactService.contacts).to.be.an('array')
-
-
     })
+
+    it('should have a property contacts, an array', function(){
+      expect(contactService.contacts).to.be.an('array');
+    });
+
+    it('should call the backend', function(){
+      $httpBackend.expectGET('http://localhost:9001/contacts')
+        .respond(200, []);
+      $httpBackend.flush();
+    });
+
   });
 });
